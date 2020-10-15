01edde60b918bbbfdcc4414cb43b86bd8411a1e9
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-10-15 21:53:02 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.addEventListener('click', (event) => {
+  if (event.target.className === 'header__menu-button') {
+    document.querySelector('.header__main-menu')
+      .className = 'header__main-menu header__main-menu--opened';
+  }
+
+  if (event.target.className === 'header__close-button') {
+    document.querySelector('.header__main-menu')
+      .className = 'header__main-menu';
+  }
+});
